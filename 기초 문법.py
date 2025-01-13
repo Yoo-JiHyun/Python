add1c459cc18b71db32b437cc04800372b927881
Yoo-JiHyun <luhyun26@gmail.com>
2025-01-13 18:28:57 +0900

    25.01.13

---
+++ 기초 문법.py
@@ -0,0 +1,99 @@
+print("hello world!")
+
+# 객체 생성
+1004
+
+# 변수, 객체 생성
+x = 1004
+x
+
+y = x
+y
+
+# id값
+id(x), id(y)
+
+# 변수 활용
+n=9
+print(n*1)
+print(n*2)
+print(n*3)
+print(n*4)
+print(n*5)
+print(n*6)
+print(n*7)
+print(n*8)
+print(n*9)
+
+# 대소문자 구분
+A = 3
+a = 1
+A,a
+
+a = 100
+print(a)
+
+"""
+여러줄 주석    
+"""
+
+# 자료형
+a = 1
+type(a)
+b = 3.14
+type(b)
+c = True
+d = False
+type(c) , type(d)
+e = "hello"
+type(e)
+
+int_value = 3
+float_value = 3.14
+bool_value = True
+str_value = "3"
+
+int(float_value)
+float(int_value)
+str(float_value)
+bool(int_value)
+bool(0)
+
+# None 값
+a = None
+a
+
+# 산술 연산자
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(5**2)
+
+# 대입 + 산술
+num = 3
+# num = num - 3
+num -= 3
+num
+
+# 비교 연산자
+a = 5
+b = 3
+print(a > b)
+print(a < b)
+print(a >= 5)
+print(b <= 3)
+print(a == b)
+print(a != b)
+
+# 논리 연산자
+1 < 3 and 3 > 1
+False or False
+False or True
+
+# 포함 연산자
+"a" in "abcd"
+"a" not in "abcd"
+
