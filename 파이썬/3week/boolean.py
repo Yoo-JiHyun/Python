aedd65fbc2907bda44a3ccbbc38b97ea5ed4c8e5
Yoo-JiHyun <luhyun26@gmail.com>
2025-03-22 16:36:12 +0900

    25.03.22 파이썬

---
+++ 파이썬/3week/boolean.py
@@ -0,0 +1,16 @@
+x = 10
+under_20 = x < 20  # under_20 = (x < 20)
+print("under_20:", under_20)
+print("not under_20:", not under_20)    # 논리연산자(not)
+
+print(10 == 100)
+print(10 < 100)
+print(10 > 100)
+print(10 <= 100)
+print(10 >= 100)
+
+print('서울' == '부산')
+print('서울' != '부산')
+print('서울' < '부산')
+print('서울' > '부산')
+# 한글은 가나다순으로 앞에있는 것이 작은 것
