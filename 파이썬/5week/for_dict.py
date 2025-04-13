f4f7e3c8f3fd06588c1b53b55e4d4a8bcd234e19
Yoo-JiHyun <luhyun26@gmail.com>
2025-04-13 15:29:47 +0900

    25.04.13

--- 파이썬/5week/for_dict.py
+++ 파이썬/5week/for_dict.py
@@ -10,3 +10,16 @@ dictionary = {
 for key in dictionary:
     # 출력합니다.
     print(key, ":", dictionary[key])
+    
+#----------------------------------------------------------------
+
+print("# 동일 요소 개수 출력")
+numbers = [1, 2, 3, 2, 1, 2, 4, 2, 3, 4, 5, 3, 2, 3, 4, 2, 3, 2, 3]
+counter = {}
+
+for number in numbers:
+    if number in counter:
+        counter[number] =+ 1
+    else:
+        counter[number] = 1
+print(counter)
--- 파이썬/5week/for_list.py
+++ 파이썬/5week/for_list.py
@@ -5,17 +5,3 @@ array = [273, 32, 103, 57, 52]
 for element in array:
     # 출력합니다.
     print(element)
-    
-    
-#----------------------------------------------------------------
-
-print("# 동일 요소 개수 출력")
-numbers = [1, 2, 3, 2, 1, 2, 4, 2, 3, 4, 5, 3, 2, 3, 4, 2, 3, 2, 3]
-counter = {}
-
-for number in numbers:
-    if number in counter:
-        counter[number] =+ 1
-    else:
-        counter[number] = 1
-print(counter)
